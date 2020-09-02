0599054392b3a3ffddbf6b25006dce0a57f8b0a0
NicolasCoelho <nicolas_mansan@hotmail.com>
2020-09-02 10:12:17 -0300

    Stylesheets added with a styling structure

--- src/ws.js
+++ src/ws.js
@@ -1,8 +1,24 @@
 var ws = (function (){
     var url = window.configs.apiUrl;
     
-    var auth = function () {
-        // TODO: fetch auth route
+    var auth = function (user, password) {
+        return mockAuth(user, password);
+    }
+
+    var mockAuth = function (user, password) {
+        var isValid = false;
+        if (
+            user === 'nicolas' && password === '321' ||
+            user === 'charles' && password === '123'
+        ) {
+            isValid = true;
+        }
+
+        var token = '';
+        if (isValid) {
+            token = atob(JSON.stringify({name: user, profile: 'user',}))+"."+atob(JSON.stringify({expDate: Date.now()}))
+        }
+        return isValid ? token : null;
     }
     
     return {
