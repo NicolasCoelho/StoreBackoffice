efd373522c8f94f7d7aa2399f4e9a20720af64cb
NicolasCoelho <nicolas_mansan@hotmail.com>
2021-01-16 02:07:48 -0300

    Landing page improved on TatianaLoureiro theme; font import dinamicly on configs file

--- autoload.js
+++ autoload.js
@@ -2,6 +2,8 @@
 (function(){
     var url = window.app.configs.staticUrl;
 
+    var fontUrl = window.app.configs.fonts;
+
     document.title = window.app.configs.appTitle;
     document.querySelector("link[rel='shortcut icon']").href = window.app.configs.favicon;
     addElement(window.app.configs.styleVars);
@@ -10,7 +12,7 @@
         'https://cdn.jsdelivr.net/npm/vue@2.6.12',
         'https://unpkg.com/vue-router@3.4.3/dist/vue-router.js',
         'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js',
-        'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap',
+        fontUrl,
         'styles/css/fontAwesome/css/all.min.css',
         'styles/css/bootstrap-grid.css',
         'styles/css/app.css',
--- configs.js
+++ configs.js
@@ -18,7 +18,8 @@ window.app = (function(){
                 width: 250,
                 height: 60
             },
-            styleVars: 'styles/css/vars.css'
+            styleVars: 'styles/css/vars.css',
+            fonts: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         },
         controllers: {}
     }
